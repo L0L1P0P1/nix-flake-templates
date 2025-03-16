c0b326420a8c4d50d54ce44eb53078b4218543aa
L0L1P0P1 <behradbadeli@gmail.com>
2025-03-16 13:15:41 +0330

    main flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+	description = "L0L1P0P's Collection of Nix Flake Templates";
+
+	outputs = { self }: {
+		mariadb = {
+			path = ./mariadb;
+			description = "MariaDB Shell";
+		};
+
+		poetry = {
+			path = ./poetry;
+			description = "Python Poetry Environment";
+		};
+
+		python = {
+			path = ./python;
+			description = "Simple Python Env";
+		};
+
+		raw = {
+			path = ./raw;
+			description = "A very basic flake";
+		};
+	};
+}
