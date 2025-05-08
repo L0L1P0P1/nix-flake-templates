e3c6a51f1d5819da85a6218a1971ed6d6f1f5b4e
L0L1P0P1 <behradbadeli@gmail.com>
2025-05-08 20:57:41 +0330

    fuck it, nixfmt on the whole repo

--- flake.nix
+++ flake.nix
@@ -1,27 +1,29 @@
 {
-	description = "L0L1P0P's Collection of Nix Flake Templates";
+  description = "L0L1P0P's Collection of Nix Flake Templates";
 
-	outputs = { self }: {
-		mariadb = {
-			path = ./mariadb;
-			description = "MariaDB Shell";
-		};
+  outputs =
+    { self }:
+    {
+      mariadb = {
+        path = ./mariadb;
+        description = "MariaDB Shell";
+      };
 
-		poetry = {
-			path = ./poetry;
-			description = "Python Poetry Environment";
-		};
+      poetry = {
+        path = ./poetry;
+        description = "Python Poetry Environment";
+      };
 
-		python = {
-			path = ./python;
-			description = "Simple Python Env";
-		};
+      python = {
+        path = ./python;
+        description = "Simple Python Env";
+      };
 
-		raw = {
-			path = ./raw;
-			description = "A very basic flake";
-		};
+      raw = {
+        path = ./raw;
+        description = "A very basic flake";
+      };
 
-		defaultTemplate = self.raw;
-	};
+      defaultTemplate = self.raw;
+    };
 }
--- raw/flake.nix
+++ raw/flake.nix
@@ -1,35 +1,34 @@
 {
-	description = "A very basic flake";
+  description = "A very basic flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-	outputs = { self, nixpkgs, ... }: 
-	let
-		system = "x86_64-linux";
-		pkgs = 	import nixpkgs { 
-			system = "${system}"; 
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = "${system}";
 
-			# Uncomment for nixpkgs configuration
-			# config = {
-				# allowBroken = false;
-			# };
-		};
-	in
-	{
-		devShells.${system}.default = 
-		pkgs.mkShell {
-			buildInputs = with pkgs; [
-				# List Of Packages:
+        # Uncomment for nixpkgs configuration
+        # config = {
+        # allowBroken = false;
+        # };
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # List Of Packages:
 
-			];
+        ];
 
-			shellHook = ''
-				# Uncomment if you use zsh
-				# zsh
-			'';
-		};
-	};
+        shellHook = ''
+          				# Uncomment if you use zsh
+          				# zsh
+          			'';
+      };
+    };
 }
-
